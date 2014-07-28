d7da24476860f879768da98d184a8364aa0e51c1
cherb <herb.christian@gmx.de>
2014-07-28 12:13:57 +0200

    Neue GUI Elemente

--- app/src/main/java/com/example/Herb/C/Ullmann/F/osmi_quiz/StartActivity.java
+++ app/src/main/java/com/example/Herb/C/Ullmann/F/osmi_quiz/StartActivity.java
@@ -12,7 +12,8 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_start);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gamec);
     }
 
 
